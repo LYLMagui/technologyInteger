95c2dfa6baed7f4812be7f2bc23e97a62a7c7240
lvMagui <2416053677@qq.com>
2022-09-17 22:35:06 +0800

    SpringBoot整合Redis

---
+++ springboot_04_redis/src/test/java/com/ukir/Springboot04RedisApplicationTests.java
@@ -0,0 +1,44 @@
+package com.ukir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+class Springboot04RedisApplicationTests {
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Test
+    void redistest() {
+        // 添加
+        redisTemplate.opsForValue().set("name","沉默王二");
+        // 查询
+        System.out.println(redisTemplate.opsForValue().get("name"));
+        // 删除
+        redisTemplate.delete("name");
+        // 更新
+        redisTemplate.opsForValue().set("name","沉默王二的狗腿子");
+        // 查询
+        System.out.println(redisTemplate.opsForValue().get("name"));
+
+        // 添加
+        stringRedisTemplate.opsForValue().set("name","沉默王二");
+        // 查询
+        System.out.println(stringRedisTemplate.opsForValue().get("name"));
+        // 删除
+        stringRedisTemplate.delete("name");
+        // 更新
+        stringRedisTemplate.opsForValue().set("name","沉默王二的狗腿子");
+        // 查询
+        System.out.println(stringRedisTemplate.opsForValue().get("name"));
+
+    }
+
+}
